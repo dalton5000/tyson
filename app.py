d346294f988b709125fed53749f112d44f051878
dalton5000 <marius.stieme@gmail.com>
2019-11-10 16:12:43 +0100

    fix syntax

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ PORT = int(os.environ.get('PORT', 8080))
 
 class Echo(DatagramProtocol):
 
-    def datagramReceived(self, data, host, port):
-        print ("received %r from %s:%d" % (data, host, port))
-        self.transport.write(data, (host, port))
+    def datagramReceived(self, datagram, address):
+        print ("received %r from %s:%d" % (datagram, address))
+        self.transport.write(datagram, address)
 
 
-reactor.listenUDP(9999, Echo())
+reactor.listenUDP(PORT, Echo())
 reactor.run()
 print("tyson is listening")
