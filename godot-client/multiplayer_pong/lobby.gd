78a90adc54e2f926bc1072bde07e7eb58c5c0b64
dalton5000 <marius.stieme@gmail.com>
2019-11-11 23:35:50 +0100

    try static port usage

--- godot-client/multiplayer_pong/lobby.gd
+++ godot-client/multiplayer_pong/lobby.gd
@@ -83,7 +83,7 @@ func start_traversal():
 	
 func host_game():
 	var host = NetworkedMultiplayerENet.new()
-	var port = udp_traversal.own_port
+	var port = DEFAULT_PORT
 	host.set_compression_mode(NetworkedMultiplayerENet.COMPRESS_RANGE_CODER)
 	var err = host.create_server(port, 1) # max: 1 peer, since it's a 2 players game
 	if err != OK:
@@ -96,7 +96,7 @@ func host_game():
 
 func join_game():
 	var ip = udp_traversal.peer_address
-	var port = udp_traversal.peer_port
+	var port = DEFAULT_PORT
 	if not ip.is_valid_ip_address():
 		dlog("IP address is invalid", false)
 		return
